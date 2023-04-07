d50e0b3484189a6068e5b2207918949703426e5b
yeabtsega45 <yabtsega2022@gmail.com>
2023-04-07 14:59:24 +0300

    setting value and onchange to the input

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -5,7 +5,7 @@ function LoginForm() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [loggedIn, setLoggedIn] = useState(false);
-  const [error, setError] = useState("");
+  const [error, setError] = useState(false);
 
   return (
     <>
@@ -14,8 +14,20 @@ function LoginForm() {
         <button>logout</button>
       </div>
       <form autoComplete="off">
-        <input type="text" placeholder="username" name="username" />
-        <input type="text" placeholder="password" name="password" />
+        <input
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          type="text"
+          placeholder="username"
+          name="username"
+        />
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="text"
+          placeholder="password"
+          name="password"
+        />
         <button>submit</button>
       </form>
     </>
